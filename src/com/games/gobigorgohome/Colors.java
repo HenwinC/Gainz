ba3aac2201cc36a4721817945fa3a8b5cbd5f444
Mannie004 <seun.adeyemi04@yahoo.com>
2022-06-22 16:11:19 -0400

    added color class

---
+++ src/com/games/gobigorgohome/Colors.java
@@ -0,0 +1,33 @@
+package com.games.gobigorgohome;
+
+import java.awt.*;
+
+public enum Colors {
+
+    RESET("</span>"),
+    CYAN("#00FFFF"),
+    MAGENTA("#FF00FF"),
+    ORANGE("#ffa500"),
+    RED("#ff0000"),
+    RED_UNDERLINED("RED_UNDERLINED"),
+    GREEN("#00FF00"),
+    YELLOW("#FFFF00"),
+    DARK_YELLOW("#8B8000"),
+    LIGHT_GREY("#A9ADAF"),
+    WHITE("#000000"),
+    BLACK("#ffffff");
+
+    private final String color;
+
+    Colors(String color) {
+        this.color = color;
+    }
+
+    public Color getRGB() {
+        return Color.decode("#FFCCEE");
+    }
+
+    public String toString() {
+        return color;
+    }
+}
