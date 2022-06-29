6b2efc1f75fd1d96d94c8a2a30697d276d12d61e
Mannie004 <seun.adeyemi04@yahoo.com>
2022-06-29 17:15:56 -0400

    workout images AB#822

--- src/com/games/gobigorgohome/app/Game.java
+++ src/com/games/gobigorgohome/app/Game.java
@@ -396,7 +396,51 @@ public class Game {
             fixBrokenMachine(targetMuscle, energyCost);
 
         }
-
+        if (playerExcerciseInput.equalsIgnoreCase("push ups")) {
+            prompter.info("<img src=\"https://res.cloudinary.com/dmrsimpky/image/upload/v1656533564/push-up_cldtxj.png\"'/>");
+        }
+        if (playerExcerciseInput.equalsIgnoreCase("ab wheel")) {
+            prompter.info("<img src=\"https://res.cloudinary.com/dmrsimpky/image/upload/v1656533563/ab-wheel_lud0bu.png\"'/>");
+        }
+        if (playerExcerciseInput.equalsIgnoreCase("dips")) {
+            prompter.info("<img src=\"https://res.cloudinary.com/dmrsimpky/image/upload/v1656533564/dips_txutdt.png\"'/>");
+        }
+        if (playerExcerciseInput.equalsIgnoreCase("row")) {
+            prompter.info("<img src=\"https://res.cloudinary.com/dmrsimpky/image/upload/v1656533564/row_zbnofo.png\"'/>");
+        }
+        if (playerExcerciseInput.equalsIgnoreCase("bike")) {
+            prompter.info("<img src=\"https://res.cloudinary.com/dmrsimpky/image/upload/v1656533563/bike_busvzq.png\"'/>");
+        }
+        if (playerExcerciseInput.equalsIgnoreCase("chest fly")) {
+            prompter.info("<img src=\"https://res.cloudinary.com/dmrsimpky/image/upload/v1656533563/chest_fly_zbhqfm.png\"'/>");
+        }
+        if (playerExcerciseInput.equalsIgnoreCase("tricep extension")) {
+            prompter.info("<img src=\"https://res.cloudinary.com/dmrsimpky/image/upload/v1656533564/Tricep_uvnrwb.png\"'/>");
+        }
+        if (playerExcerciseInput.equalsIgnoreCase("leg press")) {
+            prompter.info("<img src=\"https://res.cloudinary.com/dmrsimpky/image/upload/v1656533564/leg_press_cqxbqp.png\"'/>");
+        }
+        if (playerExcerciseInput.equalsIgnoreCase("crunch machine")) {
+            prompter.info("<img src=\"https://res.cloudinary.com/dmrsimpky/image/upload/v1656533563/crunch_machine_iq1evh.png\"'/>");
+        }
+        if (playerExcerciseInput.equalsIgnoreCase("cable curl")) {
+            prompter.info("<img src=\"https://res.cloudinary.com/dmrsimpky/image/upload/v1656533563/cable_curl_y5nztm.png\"'/>");
+        }
+        if (playerExcerciseInput.equalsIgnoreCase("squat")) {
+            prompter.info("<img src=\"https://res.cloudinary.com/dmrsimpky/image/upload/v1656533564/squat_gjoxnm.png\"'/>");
+        }
+        if (playerExcerciseInput.equalsIgnoreCase("bench press")) {
+            prompter.info("<img src=\"https://res.cloudinary.com/dmrsimpky/image/upload/v1656533563/bench_press_buaimb.png\"'/>");
+        }
+        if (playerExcerciseInput.equalsIgnoreCase("overhead press")) {
+            prompter.info("<img src=\"https://res.cloudinary.com/dmrsimpky/image/upload/v1656533564/Overhead_Press_v6i829.png\"'/>");
+        }
+        if (playerExcerciseInput.equalsIgnoreCase("deadlift")) {
+            prompter.info("<img src=\"https://res.cloudinary.com/dmrsimpky/image/upload/v1656533564/deadlift_ezow1x.png\"'/>");
+        }
+        if (playerExcerciseInput.equalsIgnoreCase("skull crushers")) {
+            prompter.info("<img src=\"https://res.cloudinary.com/dmrsimpky/image/upload/v1656533564/Skull_Crushers_t6rjzx.png\"'/>");
+        }
     }
 
     private void fixBrokenMachine(Object targetMuscle, Long energyCost) {
